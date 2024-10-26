0ba9de178424b43377f77f1f5dc487660cda04c7
endikallanomatxin <endikallano@gmail.com>
2024-10-26 21:31:23 +0200

    note down that some things won't work because I just coppied ini.lua

--- init.lua
+++ init.lua
@@ -965,6 +965,7 @@ require("lazy").setup({
 	-- require 'kickstart.plugins.neo-tree',
 	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+	-- BUG: SOLO HE COPIADO EL init.lua así que no están
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
 	--
