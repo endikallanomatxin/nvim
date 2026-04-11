5c05443712f479bb589965579841a41d86ac9a4a
Endika Llano Matxin <endikallano@gmail.com>
2026-04-11 18:01:01 +0200

    Fix markdown treesitter highlighting on Neovim 0.12

--- init.lua
+++ init.lua
@@ -982,35 +982,38 @@ require("lazy").setup({
 	},
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-				"java",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
+		config = function()
+			local ts = require("nvim-treesitter")
+			ts.setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"bash",
+					"c",
+					"diff",
+					"html",
+					"java",
+					"lua",
+					"luadoc",
+					"markdown",
+					"query",
+					"vim",
+					"vimdoc",
+				},
+				callback = function(args)
+					pcall(vim.treesitter.start, args.buf)
+					if vim.bo[args.buf].filetype ~= "ruby" then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+		end,
 		-- There are additional nvim-treesitter modules that you can use to interact
 		-- with nvim-treesitter. You should go explore a few and see what interests you:
 		--
@@ -1134,6 +1137,7 @@ require("lazy").setup({
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
 		opts = {
+			restart_highlighter = true,
 			callout = {
 				idea = {
 					raw = "[!IDEA]",
